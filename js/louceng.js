d56fac0727fdc37c0bee4edd98c26c306d36fb9d
huimin <287604368@qq.com>
2020-01-15 19:18:43 +0800

    update

--- js/louceng.js
+++ js/louceng.js
@@ -1,4 +1,5 @@
 var user = JSON.parse(localStorage.getItem("user"));
+var imgurl='http://49.235.203.189:8087/jeecg-boot/sys/common/view/';
 $(function() {
 	selectDeviceCount()
 	getloucengname()
@@ -64,10 +65,18 @@ function getloucengname() {
 			console.log(data)
 			if (data.success) {
 				let louceng = "";
+			
 				$.each(data.result, function(y, item) {
+					
+					
+					
+					if(y==0){
+						console.log("into")
+						 $("#planPic").css("background-image","url('"+imgurl+item.planPic+"')")
+					}
 					louceng += '<tr>'+
 						'<td>'+item.floorName+'</td>'+
-						'<td onclick=query("'+item.id+'")>查看</td>'+
+						'<td style="color:#6de7ff" onclick=query("'+item.id+'")>查看</td>'+
 						'</tr>';
 				})
               $("#loucengall").append(louceng)
@@ -80,6 +89,9 @@ function getloucengname() {
 	});
 }
 function query(id){
+	//更新右边的视图
+	devinlouceng(id);
+	console.log(id)
 	var req = {
 		customerId: localStorage.getItem("khid"),
 	    floorId:id
@@ -98,9 +110,92 @@ function query(id){
 	
 		},
 		success: function(data) {
-	
+	           console.log(data.result)
 			if (data.success) {
+	            
+				$("#self_all").text(data.result.deviceCount)
+				$("#self_normal").text(data.result.normalcount)
+				$("#self_whsb").text(data.result.deviceDefendCount)
+				$("#self_gzsb").text(data.result.deviceFaultCount)
+				$("#self_txgz").text(data.result.deviceCommFailCount)
+				$("#self_bjsb").text(data.result.deviceAlertCount)
+			}
 	
+		},
+		error: function() {
+			console.log("请求失败");
+		}
+	});
+}
+//中间显示的楼层设备
+function devinlouceng(id){
+	var req = {
+		fid: id,
+	   
+	}
+	$.ajax({
+	
+		url: "http://49.235.203.189:8087/jeecg-boot/monitor/floorPlan/queryFloorDeviceByFid",
+		type: "get",
+		// contentType: "application/json",
+		dataType: "json",
+		data: req,
+		beforeSend: function(request) {
+			request.setRequestHeader("X-Access-Token",
+				user.token
+			);
+	
+		},
+		success: function(data) {
+			$("#louceng_dev").empty()
+			console.log(data.result)
+			//更换楼层图纸
+			  $("#planPic").css("background-image","url('"+imgurl+data.result.picurl+"')")
+	           console.log(data.result.deviceInfoDTOS)
+			if (data.success) {
+				let sbinlouceng=""
+				let px="px"
+				// position: absolute;top: 50px;left: 30px;
+	            $.each(data.result.deviceInfoDTOS, function(y, item) {
+					let aaa=JSON.stringify(item)
+				
+					sbinlouceng+='<img onclick=sbck("' + aaa + '") style="top:'+item.yposition+px+';left:'+item.xposition+px+'" class="sbinlouceng" src="'+imgurl+item.picUrl+'" >'
+	            	// sbinlouceng+='<img onclick=sbck("'+a+'") style="top:'+item.yposition+px+';left:'+item.xposition+px+'" class="sbinlouceng" src="'+imgurl+item.picUrl+'" >'
+	            })
+			    $("#louceng_dev").append(sbinlouceng)
+			}
+	
+		},
+		error: function() {
+			console.log("请求失败");
+		}
+	});
+}
+//设备上弹出的窗口
+function sbck(obj){
+	console.log(obj)
+	var req = {
+		fid: id,
+	   
+	}
+	$.ajax({
+	
+		url: "http://49.235.203.189:8087/jeecg-boot/monitor/floorPlan/queryFloorDeviceByFid",
+		type: "get",
+		// contentType: "application/json",
+		dataType: "json",
+		data: req,
+		beforeSend: function(request) {
+			request.setRequestHeader("X-Access-Token",
+				user.token
+			);
+	
+		},
+		success: function(data) {
+			
+			
+			 
+			if (data.success) {
 				
 			}
 	
@@ -110,3 +205,4 @@ function query(id){
 		}
 	});
 }
+
--- js/sshj.js
+++ js/sshj.js
@@ -6,14 +6,38 @@ var sb1=[];
 		var sb2=[];
 		var sb3=[];
 $(function(){
-	init()
+	pagelimit(1)
 	baojing_7day()
 	baojing_1day()
 })
-function init() {
+function sure(){
+	
+	let pagemax=parseInt($("#pageall").text())
+	let topage= parseInt($("#topage").val())
+	if(topage<=pagemax){
+		pagelimit(topage)
+	}
+	
+}
+function pagedown(){
+	let current_= parseInt($("#currentpage").text()) 
+	let pagemax=parseInt($("#pageall").text())
+	if(current_<pagemax){
+		pagelimit(current_+1)
+	}
+	
+}
+function pageup(){
+	let current_= parseInt($("#currentpage").text()) 
+	if(current_>1){
+		pagelimit(current_-1)
+	}
+	
+}
+function pagelimit(no) {
 		var req = {
 			customerId: localStorage.getItem("khid"),
-			pageNo: 1,
+			pageNo: no,
 			pageSize: 8
 		}
 		$.ajax({
@@ -30,17 +54,39 @@ function init() {
 
 			},
 			success: function(data) {
-				console.log(data.result.records)
+				console.log(data)
+				$("#tbodydata").empty();
+				console.log(data)
+				var a=parseInt(data.result.total/8)
+				$("#currentpage").text(data.result.current)
+				if((data.result.total)%8==0){
+					$("#pageall").text(a)
+				}else{
+					console.log(a+1)
+					$("#pageall").text(a+1)
+				}
+				
+				
 				let tabledata=""
 				$.each(data.result.records, function(i, item) {
-					
+					let device="";
+					if(item.deviceType=="1"){
+						device="探测器"
+					}else if(item.deviceType=="2"){
+						device="控制器"
+					}else{
+						device="输出模块"
+					}
 				tabledata+=	'<tr>'+
 					   '<td>'+
 					        '<input class="check_item" type="checkbox" style="background-color: transparent;">'+
 					    '</td>'+
+						'<td>'+item.oneAreaName+'</td>'+
+						'<td>'+item.twoAreaName+'</td>'+
+						'<td>'+item.name+'</td>'+
 					    '<td>'+item.floorName+'</td>'+
 					    '<td>'+item.testDeviceImei+'</td>'+
-					    '<td>'+item.deviceType+'</td>'+
+					    '<td>'+ device+'</td>'+
 					    '<td>'+item.deviceNum+'</td>'+
 					   ' <td>'+item.detectionTarget+'</td>'+
 					    '<td>'+item.ranges+'</td>'+
@@ -137,13 +183,53 @@ function init() {
 				console.log(data)
 				let day1=[];
 				let x=[];
-				$.each(data.result.top0, function(i, item) {
-					
-					day1.push(item.count)
-					x.push(item.days.substring(item.days.length-5,item.days.length))
-					
-				})
+				if(data.success){
+					$.each(data.result.top0, function(i, item) {
+						
+						day1.push(item.count)
+						x.push(item.days.substring(item.days.length-5,item.days.length))
+						
+					})
+				}
+				
 				 dat1(day1,x)
+				
+			},
+			error: function() {
+				console.log("请求失败");
+			}
+		});
+	}
+	
+	function exporttoexel(){
+		
+		var req = {
+			customerId: localStorage.getItem("khid"),
+			acquisitionTime_begin:$("#startTime").val(),
+			acquisitionTime_end:$("#endTime").val(),
+			oneAreaId:"",//省份
+			twoAreaId:"",//城市
+			floorName:"",//楼层名称
+			deviceType:$("#deviceType").val(),//设备类型
+			deviceNum:$("#deviceNum").val(),//设备编号
+			
+		}
+		$.ajax({
+		
+			url: "http://49.235.203.189:8087/jeecg-boot/monitor/testRecord/exportXlsTestAll",
+			type: "get",
+			 // contentType: "application/json",
+			dataType: "json",
+			data: req,
+			beforeSend: function(request) {
+				request.setRequestHeader("X-Access-Token",
+					user.token
+				);
+		
+			},
+			success: function(data) {
+				console.log(data)
+				
 				
 			},
 			error: function() {
